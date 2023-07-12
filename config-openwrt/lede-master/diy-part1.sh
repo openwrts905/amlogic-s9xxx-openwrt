2775b8fd5b59b01d9aba346b03b7c03ea18030dc
openwrts905 <138843111+openwrts905@users.noreply.github.com>
2023-07-13 07:29:38 +0800

    Update diy-part1.sh

--- config-openwrt/lede-master/diy-part1.sh
+++ config-openwrt/lede-master/diy-part1.sh
@@ -6,9 +6,12 @@
 # Source code repository: https://github.com/coolsnowwolf/lede / Branch: master
 #========================================================================================================================
 
+# Uncomment a feed source
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
 # Add a feed source
- sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-# other
-# rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
-
+# Add luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+rm -rf ../lean/luci-theme-argon
